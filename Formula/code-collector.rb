7bc28412f4e7da499186cae40fd149088ec51ea9
MikhailOnyanov <mihacool3@gmail.com>
2025-12-18 13:36:58 +0300

    change checksum

--- Formula/code-collector.rb
+++ Formula/code-collector.rb
@@ -2,7 +2,7 @@ class CodeCollector < Formula
   desc "Tool for recursively collecting code to txt file for context to any LLM"
   homepage "https://github.com/MikhailOnyanov/code-collector"
   url "https://github.com/MikhailOnyanov/code-collector/archive/refs/tags/v1.0.5.tar.gz"
-  sha256 "537911a99b40606226b2658cb7d42b7fbdb5f946c4e9f3445edb1cb70c971e0d"
+  sha256 "cb7d845c06ce9f1b52c9f0100bab32e958e7ce955dfc73d4beaf63dffce532fe"
   license "MIT"
 
   depends_on "python@3.12"
